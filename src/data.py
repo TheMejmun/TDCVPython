15df67c7e2e8d2fcf0cae1fe47fc8ccaa8df37f3
Saman <saman14m@gmail.com>
2020-01-29 11:36:34 +0100

    Finished data loading

--- src/data.py
+++ src/data.py
@@ -4,32 +4,33 @@ import glob
 import re
 import os
 
+# TODO put dataset folder into project root
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 # Loads image as numpy tensor
 # Converts to RGB mode just in case
-def load_image(path):
+def __load_image(path):
     return np.array(I.open(path).convert('RGB'))
 
 
-# Stores image
-def store_image(image, path, format='PNG'):
-    image = I.fromarray(image)
-    image.save(path, format=format)
-
-
-# Path MUST end in a slash
+# folder is one of the following: 'coarse', 'fine', or 'real'
+# class_name is the name of the subfolder ('ape', 'cat',...)
+#
 # Returns a list of tuples
 # return[0] is raw image data
-# return[1] is a tuple with given pose data
-def load_folder(path):
-    image_dict = {}
+# return[1] is the class name
+# return[2] is a tuple with given pose data
+def __load_folder(folder, class_name):
+    image_dict = dict()
+    path = ROOT_DIR + '\\dataset\\' + folder + '\\' + class_name + '\\'
 
     # Get all files in path
     for f in glob.iglob(path + '*'):
         # Is image
         if re.match('.*\.png$', f):
             # Put images into dict with file name (without path) as index
-            image_dict[f[len(path):]] = load_image(f)
+            image_dict[f[len(path):]] = __load_image(f)
 
     out = list()
 
@@ -47,6 +48,64 @@ def load_folder(path):
                         float(pose_strings[3]))
 
                 out.append((image_dict[lines[i * 2][2:-1]],
+                            class_name,
                             pose))
 
     return out
+
+
+# set mode to 'all', 'test', 'train', or 'db'
+def load_dataset(mode='all'):
+    classes = ['ape', 'benchvise', 'cam', 'cat', 'duck']
+    # Split string with ', ' and put casted ints into list
+    training_split = [int(i) for i in open(ROOT_DIR + '\\dataset\\real\\training_split.txt').read().split(sep=', ')]
+
+    if mode == 'all':
+        print('Loading all datasets')
+        return {'train': load_dataset('train'),
+                'test': load_dataset('test'),
+                'db': load_dataset('db')}
+
+    elif mode == 'train':
+        print('Loading train dataset')
+
+        # Load all from 'fine' folder
+        out = list()
+        for c in classes:
+            out.extend(__load_folder('fine', c))
+
+        # Only put data from 'real' into training set, if part of training_split
+        for c in classes:
+            real_data = __load_folder('real', c)
+
+            for i in range(len(real_data)):
+                if i in training_split:
+                    # print('Appending image with index ', i, ' from ', c, ' dataset to training set')
+                    out.append(real_data[i])
+        return out
+
+    elif mode == 'test':
+        print('Loading test dataset')
+
+        out = list()
+        # Only put data from 'real' into training set, if NOT part of training_split
+        for c in classes:
+            real_data = __load_folder('real', c)
+
+            for i in range(len(real_data)):
+                if i not in training_split:
+                    # print('Appending image with index ', i, ' from ', c, ' dataset to testing set')
+                    out.append(real_data[i])
+        return out
+
+    elif mode == 'db':
+        print('Loading database dataset')
+
+        # Load all from 'coarse' folder
+        out = list()
+        for c in classes:
+            out.extend(__load_folder('coarse', c))
+        return out
+
+    else:
+        print('Error: Unknown dataset loading mode')
--- src/test.py
+++ src/test.py
@@ -2,11 +2,8 @@ import os
 import data
 
 if __name__ == '__main__':  # Only execute if called
-    dirname = os.path.dirname
-    ROOT_DIR = dirname(dirname(__file__))
-    folder = ROOT_DIR + '/dataset/fine/cat/'
 
-    data.load_folder(folder)
+    data = data.load_dataset('all')
 
     # s = 'acbdjhbfuineifnoinfioqejfiojeqoirjnhoqiwueh216.png'
     # print(int(re.findall('[0-9]+', s)[0]))
