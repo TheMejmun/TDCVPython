40aaef3a6567b6e95c8a46f065247277b663750d
Saman <saman14m@gmail.com>
2020-01-29 22:41:55 +0100

    Removed some unnecessary files and code

--- src/test.py
+++ src/test.py
@@ -1,12 +1,10 @@
 from time import time
-import os
-
-dirname = os.path.dirname
-ROOT_DIR = dirname(dirname(__file__))
+import torch
 
 if __name__ == '__main__':  # Only execute if called
     start_t = time()
+    print('Testing')
 
-    print('Hello World from ', ROOT_DIR)
+    print('CUDA is available' if torch.cuda.is_available() else 'CUDA is NOT available')
 
     print('Finished in ', round(time() - start_t, 2), 's')
---
+++ src/train.py
@@ -0,0 +1,10 @@
+from time import time
+import torch
+
+if __name__ == '__main__':  # Only execute if called
+    start_t = time()
+    print('Training')
+
+    print('CUDA is available' if torch.cuda.is_available() else 'CUDA is NOT available')
+
+    print('Finished in ', round(time() - start_t, 2), 's')
